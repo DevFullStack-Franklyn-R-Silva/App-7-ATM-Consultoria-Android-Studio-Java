ab80aa8f7804546475d92d8cecf383853b141278
Franklyn R <franklyn1998@live.com>
2022-08-24 21:06:38 -0300

    Alterando o home para principal

--- app/src/main/java/com/hadesfranklyn/atmconsultoria/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/atmconsultoria/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_servicos, R.id.nav_clientes, R.id.nav_contato, R.id.nav_sobre)
+                R.id.nav_principal, R.id.nav_servicos, R.id.nav_clientes, R.id.nav_contato, R.id.nav_sobre)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/com/hadesfranklyn/atmconsultoria/ui/principal/PrincipalFragment.java
@@ -0,0 +1,29 @@
+package com.hadesfranklyn.atmconsultoria.ui.principal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hadesfranklyn.atmconsultoria.R;
+
+
+public class PrincipalFragment extends Fragment {
+
+
+
+    public PrincipalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_principal, container, false);
+    }
+}
